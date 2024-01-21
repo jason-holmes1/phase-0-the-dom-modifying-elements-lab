224235adf2af45ece4e665d22c91f1f801b53c5b
Jason <holmesjason481@gmail.com>
2024-01-21 12:21:18 -0500

    Worked with the DOM

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+var newHeader = document.getElementById("main");
+var head = document.createElement("h1");
+newHeader= head;
+newHeader.textContent = "Jason is the champion";
+newHeader.id='victory';
